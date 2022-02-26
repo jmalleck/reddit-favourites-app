fdac2954772b98dcff079fe75472cae4474c60f7
Joel Malleck <joelmalleck@gmail.com>
2022-02-25 23:26:55 -0500

    Fix vercel error

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,10 +4,9 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 const SearchBar = (props: any) => {
   return (
     <InputGroup w="30rem">
-      <InputLeftElement
-        pointerEvents="none"
-        children={<SearchIcon color="gray.300" />}
-      />
+      <InputLeftElement pointerEvents="none">
+        <SearchIcon color="gray.300" />
+      </InputLeftElement>
       <Input type="tel" placeholder="Search Subreddit" {...props} />
     </InputGroup>
   );
